cf000c2c1af9e30f0d4388329ef02f21f6936d01
MHeMoTexHuK <daytimer253@gmail.com>
2021-12-02 16:34:06 +0300

    done

--- src/totalchaos/TotalChaos.kt
+++ src/totalchaos/TotalChaos.kt
@@ -13,22 +13,23 @@ class TotalChaos : Mod() {
 
 	init {
 		Events.on(ClientLoadEvent::class.java) {
+			//regions
 			val copy = Seq<TextureRegion>(500);
 			Core.atlas.regions.each {
 			    copy.add(TextureRegion(it));
 			};
 			
 			Core.atlas.regions.each {
-			    val index = Math.floor(Mathf.random(copy.size)).toInt();
+			    val index = Mathf.random(copy.size);
 			    it.set(copy[index] as TextureAtlas.AtlasRegion);
 			
 			    copy.remove(index);
 			};
 			
-			
+			//bundles
 			val map = Core.bundle.properties;
 			val mapCopy = map.copy();
-			val keysTemp = Seq<String>();
+			val keys = Seq<String>();
 			map.each { k, v -> keys.add(k) };
 			
 			map.each { k, v ->
@@ -41,7 +42,8 @@ class TotalChaos : Mod() {
 			
 			mapCopy.clear();
 			
-			Updater.checkUpdates();
+			//updates
+			Updater.checkUpdates(this);
 		}
 	}
 }
